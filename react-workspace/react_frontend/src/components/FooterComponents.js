2666a5341c6408ea3f211671012a0633d50618af
Anjaneyalu06 <maddikuntlaanji06@gmail.com>
2024-07-23 14:50:02 +0530

    Update FooterComponents.js

--- react-workspace/react_frontend/src/components/FooterComponents.js
+++ react-workspace/react_frontend/src/components/FooterComponents.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react'
     	return (
       	<div>
         	<footer className='footer'>
-       <span className='text-muted'>All Right Reserved 2024 &copy;NIYAZ_SIR </span>
+       <span className='text-muted'>All Right Reserved 2024 &copy;Anji</span>
         </footer>
      	 </div>
     	)
